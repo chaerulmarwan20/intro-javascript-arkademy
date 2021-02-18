fa96f125a057fbf7dd8890088f2f8083f1ead04b
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-02-18 13:55:11 +0700

    mengedit script di intro-2/no-1 & no-2

--- intro-2/no-1/script.js
+++ intro-2/no-1/script.js
@@ -5,7 +5,7 @@
  * Hasil pemanggilan method ini bertipe String.
  */
 let angka = 456.123;
-console.log(angka.toFixed(2));
+// console.log(angka.toFixed(2));
 
 // 2. random()
 /**
@@ -13,7 +13,7 @@ console.log(angka.toFixed(2));
  * Nilai akhir berada dalam rentang 0 dan 1.
  */
 let angkaAcak = Math.random();
-console.log(angkaAcak);
+// console.log(angkaAcak);
 
 // 3. concat()
 /**
@@ -21,14 +21,14 @@ console.log(angkaAcak);
  * Method ini membutuhkan 1 atau lebih argumen bertipe String untuk disambung.
  */
 let nama = "Chaerul";
-console.log(nama.concat(" Marwan"));
+// console.log(nama.concat(" Marwan"));
 
 // 4. trim()
 /**
  * Method String.trim() berfungsi untuk menghapus karakter spasi atau string kosong di awal dan di akhir String.
  */
 let alamat = " Sumedang "
-console.log(alamat.trim());
+// console.log(alamat.trim());
 
 // 5. join()
 /**
@@ -36,15 +36,15 @@ console.log(alamat.trim());
  * Sebuah argumen bisa diinputkan kedalam method ini yang nantinya berfungsi sebagai pembatas untuk menyatukan array.
  */
 let arr = [2, 5, 8, "a", "b", "c"];
-console.log(arr.join("-"));
+// console.log(arr.join("-"));
 
 // 6. reduce()
 /**
  * Method ini berfungsi untuk mereduce elemen array menjadi single value dengan menjumlahkan setiap elemen array-nya (dari kiri ke kanan)
  */
 const arrAngka = [1, 2, 3, 4, 5];
-const hasil = arrAngka.reduce((a, b) => a + b, 0);
-console.log(hasil);
+// const hasil = arrAngka.reduce((a, b) => a + b, 0);
+// console.log(hasil);
 
 // 7. every()
 /**
@@ -52,16 +52,16 @@ console.log(hasil);
  * Hasil pemanggilan method ini bertipe Boolean.
  */
 const arrNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const result = arrNumber.every(item => item > 5);
-console.log(result);
+// const result = arrNumber.every(item => item > 5);
+// console.log(result);
 
 // 8. includes()
 /**
  * Method ini berfungsi untuk mengecek apakah pada elemen array mengandung atau ada suatu karakter tertentu atau tidak.
  */
 const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const cari = number.includes(6);
-console.log(cari);
+// const cari = number.includes(6);
+// console.log(cari);
 
 // 9. find()
 /**
@@ -69,13 +69,13 @@ console.log(cari);
  * Jika yang sesuai lebih dari satu elemen, maka dia hanya akan mengambil elemen yang pertama ditemukan.
  */
 const number2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const find = number2.find(x => x > 5);
-console.log(find);
+// const find = number2.find(x => x > 5);
+// console.log(find);
 
 // 10. toString()
 /**
  * Method toString() berfungsi untuk menkonversi sebuah tipe data menjadi string. 
  */
 let nomor = 78;
-let konversi = nomor.toString();
-console.log(typeof konversi);
+// let konversi = nomor.toString();
+// console.log(typeof konversi);
--- intro-2/no-3/script.js
+++ intro-2/no-3/script.js
@@ -1,10 +1,15 @@
 const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
 	if (nilaiAwal > nilaiAkhir) {
 		return console.log("Nilai akhir harus lebih besar dari nilai awal");
+	} else if (typeof nilaiAwal !== "number" && typeof nilaiAwal !== "number") {
+		return console.log("Nilai awal dan nilai akhir harus bertipe number");
 	} else if (dataArray.length > 5) {
 		let result = dataArray.filter(item => {
 			return item >= nilaiAwal && item <= nilaiAkhir;
 		});
+		if (result.length === 0) {
+			return console.log("Tidak ada nilai yang terseleksi");
+		}
 		return console.log(result.sort((a, b) => a - b));
 	} else {
 		return console.log("Jumlah angka dalam dataArray tidak ada atau kurang dari 5");
